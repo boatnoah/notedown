f5969e5d7e39e5df6fef1c989a0f4cd9f655aa4e
Noah <noahskim7@gmail.com>
2025-04-20 17:23:48 -0700

    feat: codemirror + yjs + go websocket synced

--- frontend/src/editor.ts
+++ frontend/src/editor.ts
@@ -1,11 +1,13 @@
 import { EditorView } from "codemirror";
 import { markdown } from "@codemirror/lang-markdown";
 import { EditorState } from "@codemirror/state";
+
+import { yCollab } from "y-codemirror.next";
 import { keymap } from "@codemirror/view";
 import { insertNewline, defaultKeymap } from "@codemirror/commands";
 
 export const setupEditor = (
-  yText: any, // from yjs
+  yText: any,
   parent: HTMLElement,
   onUpdate: (markdown: string) => void,
 ) => {
@@ -14,6 +16,7 @@ export const setupEditor = (
       markdown(),
       keymap.of([...defaultKeymap, { key: "Enter", run: insertNewline }]),
       EditorView.lineWrapping,
+      yCollab(yText, null, { undoManager: false }),
       EditorView.updateListener.of((update) => {
         if (update.docChanged) {
           const text = update.state.doc.toString();
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
+import { fromUint8Array, toUint8Array } from "js-base64";
 import { setupEditor } from "./editor";
 import { marked } from "marked";
 
@@ -7,28 +7,35 @@ const socket = new WebSocket("ws://localhost:3000/ws");
 
 const ydoc = new Y.Doc();
 const yText = ydoc.getText("markdown");
-const provider = new WebsocketProvider("ws://localhost:3000", "ws", ydoc);
 
 const editorEl = document.getElementById("editor")!;
 const previewEl = document.getElementById("preview")!;
 
-const updatePreview = (markdownText: string) => {
-  previewEl.innerHTML = marked.parse(markdownText);
+const updatePreview = async (markdownText: string) => {
+  previewEl.innerHTML = await marked.parse(markdownText);
 };
 
 setupEditor(yText, editorEl, updatePreview);
+
+ydoc.on("update", (update, origin) => {
+  const documentState = Y.encodeStateAsUpdate(ydoc); // is a Uint8Array
+  const base64Encoded = fromUint8Array(documentState);
+
+  socket.send(base64Encoded);
+});
 
 socket.onopen = () => {
   console.log("Connected to the WebSocket server.");
 };
 
 socket.onmessage = (event) => {
-  console.log(event);
+  const binaryEncoded = toUint8Array(event.data);
+
+  Y.applyUpdate(ydoc, binaryEncoded);
 };
 
 socket.onclose = () => {
   console.log("WebSocket connection closed.");
-  editorEl.blur();
 };
 
 socket.onerror = (error) => {
